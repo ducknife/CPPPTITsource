1cdc205f3c36a5f3a32db8e6e56fbaa73f732d09
DasKow <154030578+ducknife@users.noreply.github.com>
2024-09-25 13:33:21 +0700

    Update CPP0232.cpp

--- CPP0232.cpp
+++ CPP0232.cpp
@@ -1,3 +1,102 @@
-#include<iostream> 
-#include<algorithm> 
-using namespace std; int main() { int t; cin >> t; while (t--) { int m, n; cin >> m >> n; int M[100][100]; for (int i = 0; i < m; i++)for (int j = 0; j < n; j++)cin >> M[i][j]; int N[100][100] = { 0 }; for (int j = 0; j < n; j++) { N[0][j] = M[0][j]; for (int i = 1; i < m; i++) if (M[i][j] == 0)N[i][j] = 0; else N[i][j] = N[i - 1][j] + 1; } for (int i = 0; i < m; i++) sort(N[i], N[i] + n); int res = 0; for (int i = 0; i < m; i++) for (int j = n - 1; j >= 0; j--) res = max(res, N[i][j] * (n - j)); cout << res << endl; } }
+#include <bits/stdc++.h>
+using namespace std;
+int maxArea(vector<vector<bool>>& mat) {
+    int R = mat.size();
+    int C = mat[0].size();
+    vector<vector<int>> hist(R, vector<int>(C, 0));
+    for (int i = 0; i < C; i++) {
+        hist[0][i] = mat[0][i];
+        for (int j = 1; j < R; j++)
+            hist[j][i] = (mat[j][i] == 0) ? 0 : hist[j - 1][i] + 1;
+    }
+    for (int i = 0; i < R; i++) {
+        vector<int> count(R + 1, 0);
+        for (int j = 0; j < C; j++)
+            count[hist[i][j]]++;
+        int col_no = 0;
+        for (int j = R; j >= 0; j--) {
+            if (count[j] > 0) {
+                for (int k = 0; k < count[j]; k++) {
+                    hist[i][col_no] = j;
+                    col_no++;
+                }
+            }
+        }
+    }
+    int curr_area, max_area = 0;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            curr_area = (j + 1) * hist[i][j];
+            max_area = max(max_area, curr_area);
+        }
+    }
+    return max_area;
+}
+int main() {
+    int t; cin >> t;
+    while (t--){
+        vector<vector<bool>> mat;
+        int n, m; cin >> n >> m;
+        for (int i = 0; i < n; i++){
+            vector<bool> tmp;
+            for (int j = 0; j < m; j++){
+                bool x; cin >> x;
+                tmp.push_back(x);
+            }
+            mat.push_back(tmp);
+        }
+        cout << maxArea(mat) << endl;
+    }
+    return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int maxArea(vector<vector<bool>>& mat) {
+    int R = mat.size();
+    int C = mat[0].size();
+    vector<vector<int>> hist(R, vector<int>(C, 0));
+    for (int i = 0; i < C; i++) {
+        hist[0][i] = mat[0][i];
+        for (int j = 1; j < R; j++)
+            hist[j][i] = (mat[j][i] == 0) ? 0 : hist[j - 1][i] + 1;
+    }
+    for (int i = 0; i < R; i++) {
+        vector<int> count(R + 1, 0);
+        for (int j = 0; j < C; j++)
+            count[hist[i][j]]++;
+        int col_no = 0;
+        for (int j = R; j >= 0; j--) {
+            if (count[j] > 0) {
+                for (int k = 0; k < count[j]; k++) {
+                    hist[i][col_no] = j;
+                    col_no++;
+                }
+            }
+        }
+    }
+    int curr_area, max_area = 0;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            curr_area = (j + 1) * hist[i][j];
+            max_area = max(max_area, curr_area);
+        }
+    }
+    return max_area;
+}
+int main() {
+    int t; cin >> t;
+    while (t--){
+        vector<vector<bool>> mat;
+        int n, m; cin >> n >> m;
+        for (int i = 0; i < n; i++){
+            vector<bool> tmp;
+            for (int j = 0; j < m; j++){
+                bool x; cin >> x;
+                tmp.push_back(x);
+            }
+            mat.push_back(tmp);
+        }
+        cout << maxArea(mat) << endl;
+    }
+    return 0;
+}
