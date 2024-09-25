51e959aa7616a83b61640b5842e94d85f53b9f67
DasKow <154030578+ducknife@users.noreply.github.com>
2024-09-25 13:46:16 +0700

    Update CPP0324_c2.cpp

--- CPP0324_c2.cpp
+++ CPP0324_c2.cpp
@@ -1,1 +1,23 @@
-
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll powmod(ll a, ll b, ll p){
+  if (b == 1) return a % p;
+  ll res = powmod(a, b / 2, p);
+  res = (res * res) % p;
+  if (b % 2) return res * a % p;
+  else return res % p;
+}
+int main(){
+    int t; cin >> t;
+    while (t--){
+        string a; cin >> a;
+        ll b, m; cin >> b >> m;
+        ll res = 0;
+        for (int i = 0; i < a.size(); i++){
+            res = res * 10 + (a[i] - '0');
+            res %= m;
+        }
+        cout << powmod(res, b, m) << endl;
+    }
+}
